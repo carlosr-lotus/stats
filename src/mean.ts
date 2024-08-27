e952ffdc9765945338d160fa6448e9a7218c6610
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-08-26 21:27:43 -0300

    ADD: mean function

--- src/mean.ts
+++ src/mean.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+// MEAN function
+// Function to return the mean of an array of numbers
+
+export function mean(arr: number[]): number {
+    if (arr.length === 0)
+        throw new Error('Cannot calculate mean of an empty array.')
+
+    if (arr.length === 1)
+        return arr[0]
+
+    const sum = arr.reduce((acc, cv) => acc + cv)
+    const mean = sum / arr.length 
+
+    return Number(mean.toFixed(2))
+}
+
+const arrTest = [1, 2, 3, 4, 5]
+const res = mean(arrTest)
+console.log('mean: ' + res)
