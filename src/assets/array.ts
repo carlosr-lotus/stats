b6f5911f0ac97d69caa669e38696e96c56d75ca8
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-08-27 22:53:24 -0300

    ADD: custom array related functions

---
+++ src/assets/array.ts
@@ -0,0 +1,7 @@
+/**
+ * Sorts an array of numbers in ascending order.
+ * @param arr - The array of numbers to sort.
+ * @returns The sorted array.
+ */
+
+export function sort(arr: number[]): number[] { return arr.sort((a, b) => a - b) }
